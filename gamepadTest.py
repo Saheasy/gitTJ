622d1de6799ce468419387fe48920bf6f224e0c1
Duck <saheasy@gmail.com>
2021-09-06 03:54:27 -0500

    updated TJ for ALice

--- gamepadTest.py
+++ gamepadTest.py
@@ -7,45 +7,27 @@ class robot:
         return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
     def __init__(self):
-        self.servoPin = 7
-        self.enaPin = 13
-        self.in1Pin = 22
-        self.in2Pin = 23
+        self.servoLeft = 8
+        self.servoRight = 9
 
         self.board = pymata4.Pymata4()
-        self.board.set_pin_mode_servo(self.servoPin)
-        self.board.set_pin_mode_pwm_output(self.enaPin)
-        self.board.set_pin_mode_digital_output(self.in1Pin)
-        self.board.set_pin_mode_digital_output(self.in2Pin)
+        self.board.set_pin_mode_servo(self.servoLeft[1])
+        self.board.set_pin_mode_servo(self.servoLeft[2])
 
     def drive(self):
-        self.board.servo_write(self.servoPin, self.map(self.leftx, 0, 255, 0, 180))
-        if self.righty >= 0:
-            self.board.digital_write(self.in1Pin, 0)
-            self.board.digital_write(self.in2Pin, 1)
-        if self.righty < 0:
-            self.board.digital_write(self.in1Pin, 1)
-            self.board.digital_write(self.in2Pin, 0)
-        self.board.pwm_write(self.enaPin, int( abs(self.righty) ) )
+        self.board.servo_write(self.servoLeft, self.map(self.leftx, 0, 255, 0, 180))
+        self.board.servo_write(self.servoRight, self.map(self.leftx, 0, 255, 0, 180))
 
     def run(self):
-        self.leftx, self.lefty, self.rightx, self.righty = 0,0,0,0
+        self.left, self.right = 0,0
         while 1:
             events = get_gamepad() #values 0-255 0 == max UP, 0 == max RIGHT
             for event in events:
                 if event.code == "ABS_Y":
-                    self.lefty = event.state
+                    self.left = event.state
                 if event.code == "ABS_RZ":
-                    self.righty = event.state
-                if event.code == "ABS_X":
-                    self.leftx = event.state
-                if event.code == "ABS_Z":
-                    self.rightx = event.state
-            print(self.leftx, end='\r')
-            self.board.pwm_write(self.enaPin, 150 )
-            self.board.digital_write(self.in1Pin, 0)
-            self.board.digital_write(self.in2Pin, 1)
-            #self.drive()
+                    self.right = event.state
+            self.drive()
 
 if __name__ == "__main__":
     jerke = robot()
